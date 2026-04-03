769c4413df0e227a8798bc7ca6a1c322717ddbb1
Yoseph Minasie <yosephm147@gmail.com>
2026-04-03 16:12:55 -0400

    Initial commit from Create Wix App

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import wix from '@wix/astro';
+import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare";
+
+export default defineConfig({
+  output: "server",
+  adapter: cloudflare(),
+  integrations: [wix(), react()],
+  image: { domains: ["static.wixstatic.com"] },
+  security: { checkOrigin: false },
+  devToolbar: { enabled: false }
+});
---
+++ src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -0,0 +1,8 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: '89f0724c-c94e-411e-bbcb-86752489fc77',
+  title: 'My Page',
+  routePath: '',
+  component: './extensions/dashboard/pages/my-page/my-page.tsx',
+});
